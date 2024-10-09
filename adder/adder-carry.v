e0080ab11b69f2ace80577f180377bc97e218789
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2024-10-09 11:22:10 +0800

    carry adder

--- adder/adder-carry.v
+++ adder/adder-carry.v
@@ -1,8 +1,8 @@
-/* ACM Class System (I) Fall Assignment 1 
+/* ACM Class System (I) Fall Assignment 1
  *
  *
  * Implement your naive adder here
- * 
+ *
  * GUIDE:
  *   1. Create a RTL project in Vivado
  *   2. Put this file into `Sources'
@@ -13,16 +13,92 @@
  *
  */
 
+
+module carry_lookahead_adder_4bit(
+        input [3:0] a,
+        input [3:0] b,
+        input cin,
+        output [3:0] sum,
+        // output cout
+		output G,
+		output P
+    );
+
+    wire [3:1] carry;
+    wire [3:0] g;
+    wire [3:0] p;
+
+    assign g = a & b;
+	assign p = a ^ b;
+
+    assign carry[1] = g[0] ^ (p[0] & cin);
+    assign carry[2] = g[1] ^ (p[1] & g[0]) ^ (p[1] & p[0] & cin);
+    assign carry[3] = g[2] ^ (p[2] & g[1]) ^ (p[2] & p[1] & g[0]) ^ (p[2] & p[1] & p[0] & cin);
+    // assign cout = g[3] ^ (p[3] & g[2]) ^ (p[3] & p[2] & g[1]) ^ (p[3] & p[2] & p[1] & g[0]) ^ (p[3] & p[2] & p[1] & p[0] & cin);
+    assign sum = a ^ b ^ {carry[3:1], cin};
+	assign G = g[3] ^ (p[3] & g[2]) ^ (p[3] & p[2] & g[1]) ^ (p[3] & p[2] & p[1] & g[0]);
+	assign P = p[3] & p[2] & p[1] & p[0];
+
+
+endmodule
+
+
 module adder(
-	// TODO: Write the ports of this module here
-	//
-	// Hint: 
-	//   The module needs 4 ports, 
-	//     the first 2 ports are 16-bit unsigned numbers as the inputs of the adder
-	//     the third port is a 16-bit unsigned number as the output
-	//	   the forth port is a one bit port as the carry flag
-	// 
+	input   [15:0]          a,
+    input   [15:0]          b,
+    output  [15:0]          sum,
+	output  cout
 );
-	// TODO: Implement this module here
-	
+	wire [3:1] carry;
+	wire [3:0] g;
+	wire [3:1] p;
+
+	carry_lookahead_adder_4bit cla0 (
+		.a(a[3:0]),
+		.b(b[3:0]),
+		.cin(1'b0),      // Initial carry-in is 0
+		.sum(sum[3:0]),
+		.G(g[0])
+		// .P(p[0]) // p[0] is not used
+	);
+
+	// assign carry[1] = g[0] ^ (p[0] & 1'b0);
+	assign carry[1] = g[0]; // cin = 0
+
+	carry_lookahead_adder_4bit cla1 (
+		.a(a[7:4]),
+		.b(b[7:4]),
+		.cin(carry[1]),        // Carry-in from previous stage
+		.sum(sum[7:4]),
+		.G(g[1]),
+		.P(p[1])
+	);
+
+	// assign carry[2] = g[1] ^ (p[1] & g[0]) ^ (p[1] & p[0] & 1'b0);
+	assign carry[2] = g[1] ^ (p[1] & g[0]);
+
+	carry_lookahead_adder_4bit cla2 (
+		.a(a[11:8]),
+		.b(b[11:8]),
+		.cin(carry[2]),        // Carry-in from previous stage
+		.sum(sum[11:8]),
+		.G(g[2]),
+		.P(p[2])
+	);
+
+	// assign carry[3] = g[2] ^ (p[2] & g[1]) ^ (p[2] & p[1] & g[0]) ^ (p[2] & p[1] & p[0] & 1'b0);
+	assign carry[3] = g[2] ^ (p[2] & g[1]) ^ (p[2] & p[1] & g[0]);
+
+	carry_lookahead_adder_4bit cla3 (
+		.a(a[15:12]),
+		.b(b[15:12]),
+		.cin(carry[3]),        // Carry-in from previous stage
+		.sum(sum[15:12]),
+		.G(g[3]),
+		.P(p[3])
+	);
+
+	// assign cout = g[3] ^ (p[3] & g[2]) ^ (p[3] & p[2] & g[1]) ^ (p[3] & p[2] & p[1] & g[0]) ^ (p[3] & p[2] & p[1] & p[0] & 1'b0);
+	assign cout = g[3] ^ (p[3] & g[2]) ^ (p[3] & p[2] & g[1]) ^ (p[3] & p[2] & p[1] & g[0]);
+
 endmodule
